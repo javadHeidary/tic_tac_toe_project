a4cf49cafcf0576cab43a7334e20beedb0418801
Javad Heidary <javadHeidary.dev@gmail.com>
2024-11-29 13:41:49 +0330

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tic/screens/home_screen.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,299 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isTurnO = true;
+
+  List<String> xOrOList = ['', '', '', '', '', '', '', '', ''];
+
+  int filledBoxes = 0;
+  bool gameHasResult = false;
+
+  int scoreX = 0;
+  int scoreO = 0;
+  String winnerTitle = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.grey[900],
+          title: Text('TicTacToe'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                clearGame();
+              },
+              icon: Icon(Icons.refresh),
+            )
+          ],
+        ),
+        backgroundColor: Colors.grey[900],
+        body: SafeArea(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              getScoreBorad(),
+              SizedBox(
+                height: 20,
+              ),
+              getResultButton(),
+              SizedBox(
+                height: 20,
+              ),
+              getGridView(),
+              getTurn(),
+            ],
+          ),
+        ));
+  }
+
+  Widget getResultButton() {
+    return Visibility(
+      visible: gameHasResult,
+      child: OutlinedButton(
+        style: OutlinedButton.styleFrom(
+          backgroundColor: Colors.red,
+          side: BorderSide(color: Colors.white, width: 2),
+        ),
+        onPressed: () {
+          setState(() {
+            gameHasResult = false;
+            clearGame();
+          });
+        },
+        child: Text(
+          '$winnerTitle, play again!',
+          style: TextStyle(
+              color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget getTurn() {
+    return Text(
+      isTurnO ? 'Turn O' : 'Turn X',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget getGridView() {
+    return Expanded(
+      child: GridView.builder(
+        itemCount: 9,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            onTap: () {
+              tapped(index);
+            },
+            child: Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+              ),
+              child: Center(
+                child: Text(
+                  xOrOList[index],
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: xOrOList[index] == 'X' ? Colors.white : Colors.red,
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void tapped(int index) {
+    if (gameHasResult) {
+      return;
+    }
+
+    setState(() {
+      if (xOrOList[index] != '') {
+        return;
+      }
+
+      if (isTurnO) {
+        xOrOList[index] = 'O';
+        filledBoxes = filledBoxes + 1;
+      } else {
+        xOrOList[index] = 'X';
+        filledBoxes = filledBoxes + 1;
+      }
+
+      isTurnO = !isTurnO;
+
+      checkWineer();
+    });
+  }
+
+  void checkWineer() {
+    if (xOrOList[0] == xOrOList[1] &&
+        xOrOList[0] == xOrOList[2] &&
+        xOrOList[0] != '') {
+      setResult(xOrOList[0], 'winner is ' + xOrOList[0]);
+      return;
+    }
+
+    if (xOrOList[3] == xOrOList[4] &&
+        xOrOList[3] == xOrOList[5] &&
+        xOrOList[3] != '') {
+      setResult(xOrOList[3], 'winner is ' + xOrOList[3]);
+
+      return;
+    }
+
+    if (xOrOList[6] == xOrOList[7] &&
+        xOrOList[6] == xOrOList[8] &&
+        xOrOList[6] != '') {
+      setResult(xOrOList[6], 'winner is ' + xOrOList[6]);
+
+      return;
+    }
+
+    if (xOrOList[0] == xOrOList[3] &&
+        xOrOList[0] == xOrOList[6] &&
+        xOrOList[0] != '') {
+      setResult(xOrOList[0], 'winner is ' + xOrOList[0]);
+      return;
+    }
+
+    if (xOrOList[1] == xOrOList[4] &&
+        xOrOList[1] == xOrOList[7] &&
+        xOrOList[1] != '') {
+      setResult(xOrOList[1], 'winner is ' + xOrOList[1]);
+      return;
+    }
+
+    if (xOrOList[2] == xOrOList[5] &&
+        xOrOList[2] == xOrOList[8] &&
+        xOrOList[2] != '') {
+      setResult(xOrOList[2], 'winner is ' + xOrOList[2]);
+      return;
+    }
+
+    if (xOrOList[0] == xOrOList[4] &&
+        xOrOList[0] == xOrOList[8] &&
+        xOrOList[0] != '') {
+      setResult(xOrOList[0], 'winner is ' + xOrOList[0]);
+      return;
+    }
+
+    if (xOrOList[2] == xOrOList[4] &&
+        xOrOList[2] == xOrOList[6] &&
+        xOrOList[2] != '') {
+      setResult(xOrOList[2], 'winner is ' + xOrOList[2]);
+      return;
+    }
+
+    if (filledBoxes == 9) {
+      setResult('', 'Draw');
+    }
+  }
+
+  Widget getScoreBorad() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '$scoreO',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Text(
+                  '$scoreX',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void setResult(String winner, String title) {
+    setState(() {
+      gameHasResult = true;
+      winnerTitle = title;
+      if (winner == 'X') {
+        scoreX = scoreX + 1;
+      } else if (winner == 'O') {
+        scoreO = scoreO + 1;
+      } else {
+        scoreX = scoreX + 1;
+        scoreO = scoreO + 1;
+      }
+    });
+  }
+
+  void clearGame() {
+    setState(() {
+      for (int i = 0; i < xOrOList.length; i++) {
+        xOrOList[i] = '';
+      }
+    });
+
+    filledBoxes = 0;
+  }
+}
